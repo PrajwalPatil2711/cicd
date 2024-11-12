343f82340b983162a2652ff58bcaae6c3f975166
PrajwalPatil2711 <patilprajwal150@gmail.com>
2024-11-12 13:49:46 +0530

    first commit

---
+++ jobs/spark_job.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+
+def main():
+    spark = SparkSession.builder.appName("ExampleJob").getOrCreate()
+    data = [("Alice", 34), ("Bob", 45), ("Cathy", 29)]
+    df = spark.createDataFrame(data, ["Name", "Age"])
+    df.show()
+
+if __name__ == "__main__":
+    main()
